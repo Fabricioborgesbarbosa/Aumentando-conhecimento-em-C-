6aa231013610171ec31437023af5bc38e925b25a
Fabrício Borges Barbosa <102926773+Fabricioborgesbarbosa@users.noreply.github.com>
2022-10-20 15:56:42 -0300

    Manipulando Array em C Sharp.
    Neste arquivo eu coloquei um pouco  de meu conhecimento em Array em C Sharp

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿/* 
+   For e foreach array inteiro simples:
+*/
+
+int[] arrayInt = new int[3];
+arrayInt[0] = 18;
+arrayInt[1] = 20;
+arrayInt[2] = 22;
+
+//Redimerecionando array, Resize = copia do array.
+//Array.Resize(ref arrayInt, arrayInt.Length * 3);
+
+//Copiando um array
+//criando um novo array, aumentando suaa capacidade
+int[] arrayIntDobrado = new int[arrayInt.Length * 3];
+Array.Copy(arrayInt, arrayIntDobrado, arrayInt.Length );
+
+
+Console.WriteLine("\nfor:");
+for (int i = 0; i < arrayInt.Length; i++)
+{
+
+    Console.WriteLine($"Posição numero {i} =  {arrayInt[i]}");
+
+}
+
+
+Console.WriteLine("\nforeach:");
+int contador = 0;
+foreach (int valor in arrayInt)
+{
+    Console.WriteLine($"Posição Numero: {contador} = {arrayInt[contador]}");
+    contador++;
+}
